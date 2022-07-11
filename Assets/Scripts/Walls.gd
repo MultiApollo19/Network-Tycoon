c7b9113e724ab73e2e3798bd5a5b8067055e59cf
MultiApollo19 <mariusz.belwon@gmail.com>
2022-07-11 14:23:24 +0200

    Camera prep
    Preparation step for camera rotation

---
+++ Assets/Scripts/Walls.gd
@@ -0,0 +1,17 @@
+extends Camera
+
+var detector = null
+
+func _ready():
+	detector = get_node("WallDetector")
+func _process(_delta):
+	pass
+	#detect_colision()
+	
+#func detect_colision():	
+	#var walls = detector.get_overlapping_bodies()
+	#for i in walls.size():
+		#walls[i].active = false;
+		
+func _on_WallDetector_body_entered(body):
+	body.get_parent().queue_free()
